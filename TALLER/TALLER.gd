a8962f96acb04cf9f19d7198b626d96b91771ab1
CiroWS <cwendlersavino@gmail.com>
2026-06-26 09:05:35 -0300

    Autos y parte del taller.update67

--- TALLER/TALLER.gd
+++ TALLER/TALLER.gd
@@ -3,6 +3,16 @@ extends Control
 signal taller
 signal outaller
 
+var precio :  int
+
+
+func _physics_process(delta):
+	if precio>0:
+		$VBoxContainer2/Button.text = "Precio" + "\n" + str(precio)
+	else:
+		$VBoxContainer2/Button.text = "El auto no necesita reparse"
+	precio_ctrl()
+
 func _input(event):
 	if event.is_action_pressed("accion"):
 		get_tree().change_scene("res://mapa.tscn")
@@ -11,4 +21,10 @@ func _input(event):
 
 func _ready():
 	emit_signal("taller")
+
+func precio_ctrl():
+	var roto = $Auto.vida_auto
+	precio=(100-$Auto.vida_auto)*10
+		
+  
 
