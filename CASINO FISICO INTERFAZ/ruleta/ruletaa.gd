ba61d7a79097dc165fec71d7573d5d2f48d7ea8d
Rodimir909 <ignacioch909@gmail.com>
2026-07-24 11:34:55 -0300

    jgtyik

--- CASINO FISICO INTERFAZ/ruleta/ruletaa.gd
+++ CASINO FISICO INTERFAZ/ruleta/ruletaa.gd
@@ -1,29 +1,7 @@
 extends Control
 
-var dale:bool=false
-var c:int
-var jeje:int
-
-
+var posicion : Vector2
 
 func _ready():
-	girar()
-
-func girar():
-	jeje=GLOBAL.random(5,50)
-	
-func _physics_process(delta):
-	if dale==true:
-		c+=jeje
-		$Sprite2.rotation_degrees=c
-		if c>=360:
-			c=0
-			jeje=jeje/2
-			print("ay")
-		elif jeje<=1:
-			girar()
-			dale=false
-	print(jeje)
-			
-func _on_Button_pressed():
-	dale=true
+	posicion=$VBoxContainer/fila11/treintaydos.rect_global_position
+	print(posicion.x+59,"-", posicion.y+38)
