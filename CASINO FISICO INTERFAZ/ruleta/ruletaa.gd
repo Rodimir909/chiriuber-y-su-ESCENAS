aec726b8b841000c29b45773840232a085d39151
Rodimir909 <178239450+Rodimir909@users.noreply.github.com>
2026-07-31 05:14:06 -0300

    fafa

--- CASINO FISICO INTERFAZ/ruleta/ruletaa.gd
+++ CASINO FISICO INTERFAZ/ruleta/ruletaa.gd
@@ -1,10 +1,16 @@
 extends Control
 
+onready var label = [$apuesta1, $apuesta2, $apuesta3]
+
+var boton = []
+
 var posicion : Vector2
 
 var alea : int
 
 var n : int = 200
+
+var h = [0, 0, 0]
 
 var hab : bool = true
 
@@ -19,7 +25,6 @@ func _ready():
 	for boton in get_tree().get_nodes_in_group("apuesta"):
 		# Conectamos la señal "pressed" de cada botón a UNA SOLA función
 		# Y le pasamos el propio botón como argumento (BIND)
-		print(boton)
 		boton.connect("pressed", self, "_on_boton_apuesta_pressed", [boton])
 	
 func _physics_process(delta):
@@ -40,26 +45,65 @@ func _physics_process(delta):
 				c=0
 			$ruleta.rotation_degrees=c
 			if c==numruleta[alea]:
+				for i in range(boton.size()):
+					label[i].visible=false
 				n=0
 				hab=true
 				cant=10
+				h=[0, 0 ,0]
+				boton=[]
+
 		else:
 			cant-=0.01
 			c+=cant
 			if c>=360:
 				c=0
 			$ruleta.rotation_degrees=c
+		$limite.visible=false
 
 func _on_boton_apuesta_pressed(boton_presionado: Button):
 	# Obtenemos el nombre del nodo directamente (ej: "01", "02", "RED", "BLACK")
-	var id_casillero : Vector2 = boton_presionado.rect_global_position
-	id_casillero.x=id_casillero.x+47
-	id_casillero.y=id_casillero.y+25
-	$apuesta1.visible=true
-	$apuesta1.rect_global_position=id_casillero
-	print("Se presionó el botón: ", id_casillero)
-
-
+	if hab==true:
+		var gg=false
+		for i in range(boton.size()):
+			if str(boton_presionado)==str(boton[i]):
+				if h[i]<10:
+					h[i]+=1
+					label[i].text=str("x", h[i])
+				else:
+					if i==0:
+						label[0].visible=false
+						boton[0]=boton[2]
+						h[0]=h[2]
+						label[0]=label[2]
+						label[0].text=str("x", h[0])
+						boton.remove(2)
+						h[2]=0
+						
+					elif i==1:
+						label[1].visible=false
+						boton[1]=boton[2]
+						h[1]=h[2]
+						label[1]=label[2]
+						label[1].text=str("x", h[1])
+						boton.remove(2)
+					else:
+						boton.remove(2)
+				
+				gg=true
+				break
+	#suma
+		if gg==false:
+			if boton.size()<3:
+				boton.append(boton_presionado)
+				var id_casillero : Vector2 = boton_presionado.rect_global_position
+				id_casillero.x=id_casillero.x+47
+				id_casillero.y=id_casillero.y+25
+				label[(boton.size())-1].visible=true
+				label[(boton.size())-1].rect_global_position=id_casillero
+			else:
+				$limite.visible=true
+		print(boton)
 
 func _on_Button_pressed():
 	alea=GLOBAL.random(0,36)
