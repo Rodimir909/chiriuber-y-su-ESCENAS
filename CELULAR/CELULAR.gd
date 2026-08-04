959b0edb32a904e07f90b22104c12d3a933193cd
Rodimir909 <ignacioch909@gmail.com>
2026-08-04 08:06:46 -0300

    interfazzzzzz

--- CELULAR/CELULAR.gd
+++ CELULAR/CELULAR.gd
@@ -12,3 +12,24 @@ func _on_NAVECITA_pressed():
 
 func _on_TRAEYO_pressed():
 	get_tree().change_scene("res://CELULAR/TRAEYO/TRAEYO.tscn")
+
+
+func _on_MISIONES_pressed():
+	get_tree().change_scene("res://CELULAR/MISIONES/MISIONES.tscn")
+
+
+
+func _on_NERMARKET_pressed():
+	get_tree().change_scene("res://CELULAR/NETMARKET/NETMARKET.tscn")
+
+
+func _on_SPIFY_pressed():
+	get_tree().change_scene("res://CELULAR/SPIFY/SPIFY.tscn")
+
+
+func _on_DIUB_pressed():
+	get_tree().change_scene("res://CELULAR/DIUB/DIUB.tscn")
+
+
+func _on_WASP_pressed():
+	get_tree().change_scene("res://CELULAR/WASP/WASP.tscn")
